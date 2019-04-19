6739ddddf71c0af9c6339736320d171a1a905fa7
GiuseppeCagnazz <giuseppe.cagnazzo@gmail.com>
2019-04-19 13:58:42 +0200

    added base files

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about">
+        <h1>Hello {{ their_name }}, This is an about page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, esse mollitia! Labore autem ratione asperiores accusantium dolor consequuntur quos assumenda?</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      their_name: this.$route.params.name
+    }
+  }
+}
+</script>
+
